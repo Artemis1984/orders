dbe004fa5a8d4d31d48e671e68413d58e33d2466
artak <artak-minasyan-84@mail.ru>
2021-01-04 22:16:20 +0300

    Изменение структуры заказов, создание аналогичных групп в других магазинах

---
+++ create_groups.py
@@ -0,0 +1,206 @@
+import json
+from pprint import pprint
+from itertools import groupby
+
+
+def read_file(file_name):
+    with open(file_name) as f:
+        file = f.read()
+        file = json.loads(file)
+        f.close()
+    return file
+
+
+def write_file(file_name, data):
+    with open(file_name, 'w') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+        f.close()
+    return
+
+
+# order = [{'name': 'Шампанское Moet & Chandon, Brut "Imperial", gift box',
+#          'Артикул': 'в2752',
+#          'image': 'https://s.wine.style/images_gen/275/2752/0_0_prod_desktop.jpg',
+#          'link': 'https://winestyle.ru/products/Moet-Chandon-Brut-Imperial-in-gift-box.html',
+#          'Объем': 0.75,
+#          'quantity': 2,
+#          'section': 'Шампанское и игристое вино',
+#          'product_id': '8361938',
+#          'spider': 'Winestyle',
+#          'price': 3986},
+#          {'name': 'Виски "Macallan" Double Cask 12 Years Old, gift box, 0.7 л',
+#          'Артикул': 'в71280',
+#          'image': 'https://s.wine.style/images_gen/712/71280/0_0_prod_desktop.jpg',
+#          'link': 'https://winestyle.ru/products/Macallan-Double-Cask-12-Years-Old-gift-box.html',
+#          'Объем': 0.7,
+#          'quantity': 3,
+#          'section': 'Виски',
+#          'product_id': '6290152',
+#          'spider': 'Winestyle',
+#          'price': 4576},
+#          {'name': 'Виски "Jameson", 0.7 л',
+#          'Артикул': 'в14220',
+#          'image': 'https://s.wine.style/images_gen/142/14220/0_0_prod_desktop.jpg',
+#          'link': 'https://winestyle.ru/products/Jameson-700.html',
+#          'Объем': 0.7,
+#          'quantity': 1,
+#          'section': 'Виски',
+#          'product_id': '8388019',
+#          'spider': 'Winestyle',
+#          'price': 1412}
+# ]
+
+# Нужно обговорить в каком формате я буду получать заказ
+def take_other_groups():
+    order = read_file('order.json')
+
+    order_num = order[0]['order_num']
+    order = order[0][order[0]['active']]
+
+    # print(order[0])
+
+    champagne = read_file('champagne.json')
+    whiskey = read_file('whiskey.json')
+    vodka = read_file('vodka.json')
+    cognac = read_file('cognac.json')
+
+    cats = {'Виски': whiskey, 'Шампанское и игристое вино': champagne, 'Водка': vodka, 'Коньяк': cognac}
+
+    other_groups = []
+
+    sections = set([i['section'] for i in order])
+
+    for item in order:
+        # Исключаем поставщика, которому принадлежит ордер, но в случае смешанного заказа, нужно будет не исключать и взять всех
+        others = [i for i in cats[item['section']] if 'product_id' in i.keys() and (i['product_id'] == item['product_id']) and (i['spider'] != item['spider']) and (i['features']['Объем'] == item['Объем'])]
+        for k in others:
+            other_groups.append(k)
+
+    keyfunc = lambda x: x['spider']
+
+    grouped_shops = list(set([i[0] for i in groupby(other_groups, key=keyfunc)]))
+
+    new_groups = {}
+    items = []
+
+    for i in grouped_shops:
+        found = [k for k in other_groups if k['spider'] == i]
+        sect = len(set([r['section'] for r in found]))
+        if sect == len(sections):
+            new_groups[i] = found
+
+
+    updated_groups = {}
+    for i in new_groups:
+        updated_groups[i] = []
+        for j in new_groups[i]:
+            new_dict = {}
+            new_dict['price'] = j['price']
+            new_dict['name'] = j['name']
+            new_dict['image'] = j['image']
+            new_dict['link'] = j['link']
+            new_dict['product_id'] = j['product_id']
+            new_dict['Объем'] = j['features']['Объем']
+            if 'Артикул' in j.keys():
+                new_dict['Артикул'] = j['article']
+            quantity = [k['quantity'] for k in order if k['product_id'] == j['product_id']][0]
+            new_dict['quantity'] = quantity
+            section = [k['section'] for k in order if k['product_id'] == j['product_id']][0]
+            new_dict['section'] = section
+            new_dict['spider'] = j['spider']
+            updated_groups[i].append(new_dict)
+
+
+    for i in updated_groups:
+        products = [k['product_id'] for k in updated_groups[i]]
+        product_id_list = []
+        unique_products = []
+        if len(products) > len(set(products)):
+            for j in set(products):
+                min_price = min([n['price'] for n in updated_groups[i] if n['product_id'] == j])
+                unique_products.append([r for r in updated_groups[i] if r['price'] == min_price and r['product_id'] == j][0])
+            updated_groups[i] = unique_products
+        else:
+            unique_products.append({'order': updated_groups[i]})
+
+    # min_price = 0
+    # for i in updated_groups:
+    #     min_ = sum([k['price'] for k in updated_groups[i]])
+    #     min_ = float(min_) if '.' in str(min_) else int(min_)
+    #     if min_price < min_:
+    #         min_price = min_
+    #
+    # for i in updated_groups:
+    #     if sum([k['price'] for k in updated_groups[i]]) == min_price:
+    #         order = [k for k in updated_groups[i]]
+
+
+    # updated_groups['order_num'] = '1872658'
+
+    # write_file('order.json', order)
+
+    # pprint(updated_groups)
+
+    if len(set([i['spider'] for i in order])) == 1:
+        # print('in it')
+        updated_groups[order[0]['spider']] = {'order': order, 'confirmed': False}
+        # updated_groups['confirmed'] = dict()
+        # for i in updated_groups.keys():
+        #     updated_groups['confirmed'][i] = False
+        # print(order[0]['spider'])
+        # updated_groups['confirmed'].pop('confirmed')
+        updated_groups['active'] = order[0]['spider']
+
+    # pprint([i for i in updated_groups.keys()])
+
+    updated_groups['order_num'] = order_num
+    for i in updated_groups:
+        if type(updated_groups[i]) != dict and i in grouped_shops:
+            # print(i, updated_groups[i])
+            updated_groups[i] = {'order': updated_groups[i], 'confirmed': False}
+
+    return updated_groups
+
+
+# pprint(take_other_groups())
+
+
+
+# [
+#   {
+#     "price": 1932,
+#     "name": "Виски ирландский «Jameson», 0.7 л",
+#     "image": "https://static.winestreet.ru/off-line/goods_file/31984/file_L.jpg",
+#     "link": "https://winestreet.ru/wiskey/jameson/5011007003005.html",
+#     "product_id": "8388019",
+#     "Объем": 0.7,
+#     "quantity": 1,
+#     "section": "Виски",
+#     "spider": "Winestreet"
+#   },
+#   {
+#     "price": 4637,
+#     "name": "Виски шотландский «Macallan Double Cask 12 Years Old» в подарочной упаковке",
+#     "image": "https://static.winestreet.ru/off-line/goods_file/42654/file_L.jpg",
+#     "link": "https://winestreet.ru/wiskey/macallan/goods_72814-macallan-double-cask-12-years-old-v-podarochno-upakovke.html",
+#     "product_id": "6290152",
+#     "Объем": 0.7,
+#     "quantity": 3,
+#     "section": "Виски",
+#     "spider": "Winestreet"
+#   },
+#   {
+#     "price": 6121,
+#     "name": "Шампанское белое брют «Moet & Chandon Imperial», 0.75 л",
+#     "image": "https://static.winestreet.ru/off-line/goods_file/5721/file_L.jpg",
+#     "link": "https://winestreet.ru/champagne-and-sparkling-wines/moet-chandon/asto2263.html",
+#     "product_id": "8361938",
+#     "Объем": 0.75,
+#     "quantity": 2,
+#     "section": "Шампанское и игристое вино",
+#     "spider": "Winestreet"
+#   }
+# ]
+
+def test():
+    pass
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 from pymongo import MongoClient
 from pprint import pprint
 from hashlib import sha1
+from create_groups import read_file
+from create_groups import take_other_groups
 
 
 client = MongoClient('localhost', 27017)
@@ -10,50 +12,75 @@ Orders_DB = client.Orders_DB
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '12091988BernardoProvencanoToto'
 
-order = [{'name': 'Шампанское Moet & Chandon, Brut "Imperial", gift box',
-         'Артикул': 'в2752',
-         'image': 'https://s.wine.style/images_gen/275/2752/0_0_prod_desktop.jpg',
-         'link': 'https://winestyle.ru/products/Moet-Chandon-Brut-Imperial-in-gift-box.html',
-         'Объем': 0.7,
-         'quantity': 2,
-         'price': 3986},
-         {'name': 'Виски "Macallan" Double Cask 12 Years Old, gift box, 0.7 л',
-         'Артикул': 'в71280',
-         'image': 'https://s.wine.style/images_gen/712/71280/0_0_prod_desktop.jpg',
-         'link': 'https://winestyle.ru/products/Macallan-Double-Cask-12-Years-Old-gift-box.html',
-         'Объем': 0.7,
-         'quantity': 3,
-         'price': 4576}
-         ]
+# order = [{'name': 'Шампанское Moet & Chandon, Brut "Imperial", gift box',
+#          'Артикул': 'в2752',
+#          'image': 'https://s.wine.style/images_gen/275/2752/0_0_prod_desktop.jpg',
+#          'link': 'https://winestyle.ru/products/Moet-Chandon-Brut-Imperial-in-gift-box.html',
+#          'Объем': 0.75,
+#          'quantity': 2,
+#          'type': 'Шампанское',
+#          'price': 3986},
+#          {'name': 'Виски "Macallan" Double Cask 12 Years Old, gift box, 0.7 л',
+#          'Артикул': 'в71280',
+#          'image': 'https://s.wine.style/images_gen/712/71280/0_0_prod_desktop.jpg',
+#          'link': 'https://winestyle.ru/products/Macallan-Double-Cask-12-Years-Old-gift-box.html',
+#          'Объем': 0.7,
+#          'quantity': 3,
+#          'type': 'Виски',
+#          'price': 4576}
+# ]
 
 
+# print(order)
 
-order_num = '1872658'
-orders = [order]
-
+# order_num = '1872658'
 
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
+    order = read_file('order.json')
+    order_num = order[0]['order_num']
+    shop = order[0]['active']
+    order = order[0][order[0]['active']]
+    sum_ = sum([i['price'] * i['quantity'] for i in order])
 
-    # print('Запрос', request.args)
+    order = {'_id': order_num, 'link': 'http://127.0.0.1:5000/order/' + order_num, 'sum_': sum_,
+             'status': 'Ожидает', 'order_shop': shop}
+
+    Orders_DB['orders'].update_one({'_id': take_other_groups()['order_num']}, {'$set': take_other_groups()}, upsert=True)
+    # pprint(list(Orders_DB['orders'].find({'_id': take_other_groups()['order_num']})))
+    # print('from DB')
+    return render_template('main_page.html', orders=[order])
+
+
+@app.route('/order/<order_num>', methods=['GET', 'POST'])
+def orders(order_num):
+    # order = read_file('order.json')
+    # pprint(list(Orders_DB['orders'].find()))
+    order = list(Orders_DB['orders'].find({'_id': order_num}))
+    order_num = order[0]['order_num']
+    order = order[0][order[0]['active']]
+    orders = [order]
+
     ip_address = request.remote_addr.replace('.', '')
     # Orders_DB['orders'].delete_many({})
     # Orders_DB['orders'].update_one({'_id': order_num}, {'$set': {'order': order}}, upsert=True)
     # Orders_DB['orders'].update_one({'_id': order_num}, {'$set': {'confirmed': False}}, upsert=True)
 
     if 'comment' in request.form:
+        active_shop = list(Orders_DB['orders'].find({'_id': order_num}))[0]['active']
         if request.form['comment']:
-            item = list(Orders_DB['orders'].find({'_id': order_num}, {'comments': 1}))
-            # print('UP', comments)
-            if 'comments' in item[0].keys():
-                comments = item[0]['comments']
+            item = list(Orders_DB['orders'].find({'_id': order_num}, {active_shop+'.comments': 1}))
+            if 'comments' in item[0][active_shop].keys():
+                comments = item[0][active_shop]['comments']
+                print('UP', comments)
                 # comments.append(request.form['comment'])
                 comments.append({ip_address: request.form['comment']})
             else:
                 comments = []
                 # comments.append(request.form['comment'])
                 comments.append({ip_address: request.form['comment']})
-            Orders_DB['orders'].update_one({'_id': order_num}, {'$set': {'comments': comments}})
+
+            Orders_DB['orders'].update_one({'_id': order_num}, {'$set': {active_shop+'.comments': comments}})
 
     if 'not_in_stock' in request.form:
         found = list(Orders_DB['orders'].find({'_id': order_num}))
@@ -67,6 +94,7 @@ def main_page():
                             i.pop('not_in_stock')
                 Orders_DB['orders'].delete_one({'_id': order_num})
                 Orders_DB['orders'].update_one({'_id': order_num}, {'$set': found[0]}, upsert=True)
+        # pprint(found)
 
     if 'saving' in request.form:
         orders = list(Orders_DB['orders'].find({'_id': order_num}))
@@ -84,19 +112,25 @@ def main_page():
         Orders_DB['orders'].delete_one({'_id': order_num})
         Orders_DB['orders'].update_one({'_id': order_num}, {'$set': orders[0]}, upsert=True)
 
+    # pprint(list(Orders_DB['orders'].find({'_id': order_num})))
+
     orders = list(Orders_DB['orders'].find({'_id': order_num}))
-    orders = [orders[0]['order']]
+    orders = [orders[0][orders[0]['active']]['order']]
     sum_ = sum([i['price'] * i['quantity'] for i in orders[0] if not ('not_in_stock' in i.keys())])
+    # pprint(list(Orders_DB['orders'].find({'_id': order_num})))
+
+    confirmed = list(Orders_DB['orders'].find({'_id': order_num}))[0]
+    confirmed = confirmed[confirmed['active']]['confirmed']
+    shop = list(Orders_DB['orders'].find({'_id': order_num}))[0]['active']
+
+    items = list(Orders_DB['orders'].find({'_id': order_num}))
+    comments = []
+    if 'comments' in items[0][shop].keys():
+        comments = items[0][shop]['comments']
+
     pprint(list(Orders_DB['orders'].find({'_id': order_num})))
 
-    items = list(Orders_DB['orders'].find({'_id': order_num}, {'comments': 1}))
-    comments = []
-    if 'comments' in items[0].keys():
-        comments = items[0]['comments']
-
-    confirmed = list(Orders_DB['orders'].find({'_id': order_num}))[0]['confirmed']
-
-    return render_template('orders.html', groups=orders, order_num=order_num, sum_=sum_, comments=comments[::-1], my_ip=ip_address, confirmed=confirmed)
+    return render_template('orders.html', groups=orders, order_num=order_num, sum_=sum_, comments=comments[::-1], my_ip=ip_address, confirmed=confirmed, shop=shop)
 
 
 if __name__ == '__main__':
