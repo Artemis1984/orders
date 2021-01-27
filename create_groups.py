9873cde02b6765fb58b31fdf0f10e82aa19500ce
artak <artak-minasyan-84@mail.ru>
2021-01-27 22:01:34 +0300

    внесены поправки для получения других ТП

--- create_groups.py
+++ create_groups.py
@@ -1,6 +1,7 @@
 import json
 from pprint import pprint
 from itertools import groupby
+import requests
 
 
 def read_file(file_name):
@@ -31,14 +32,17 @@ def take_other_groups(order):
 
     cats = {'Виски': whiskey, 'Шампанское и игристое вино': champagne, 'Водка': vodka, 'Коньяк': cognac}
     other_groups = []
-    sections = set([i['section'] for i in order])
+    # sections = set([i['section'] for i in order])
+    order_ids = set([i['product_id'] for i in order])
 
     for item in order:
         # Исключаем поставщика, которому принадлежит ордер, но в случае смешанного заказа, нужно будет не исключать и взять всех
         # others = [i for i in cats[item['section']] if 'product_id' in i.keys() and (i['product_id'] == item['product_id']) and (i['spider'] != item['spider']) and (i['features']['Объем'] == item['Объем'])]
-        others = [i for i in cats[item['section']] if 'product_id' in i.keys() and (i['product_id'] == item['product_id']) and (i['features']['Объем'] == item['Объем'])]
+        others = [i for i in cats[item['section']] if 'product_id' in i.keys() and (i['product_id'] == item['product_id']) and ('Объем' in i['features'].keys() and i['features']['Объем'] == item['Объем'])]
         for k in others:
             other_groups.append(k)
+
+    # pprint(other_groups)
 
     keyfunc = lambda x: x['spider']
     grouped_shops = list(set([i[0] for i in groupby(other_groups, key=keyfunc)]))
@@ -46,8 +50,14 @@ def take_other_groups(order):
 
     for i in grouped_shops:
         found = [k for k in other_groups if k['spider'] == i]
-        sect = len(set([r['section'] for r in found]))
-        if sect == len(sections):
+        # sect = len(set([r['section'] for r in found]))
+        # found_ids = len(set([i['product_id'] for i in found]))
+        # if found_ids == len(order_ids):
+        pprint(found)
+        for item in order:
+            if not [k for k in found if k['product_id'] == item['product_id'] and (k['features']['Объем'] == item['Объем'])]:
+                break
+        else:
             new_groups[i] = found
 
     updated_groups = {}
@@ -74,9 +84,13 @@ def take_other_groups(order):
         products = [k['product_id'] for k in updated_groups[i]]
         unique_products = []
         if len(products) > len(set(products)):
-            for j in set(products):
-                min_price = min([n['price'] for n in updated_groups[i] if n['product_id'] == j])
-                unique_products.append([r for r in updated_groups[i] if r['price'] == min_price and r['product_id'] == j][0])
+            # for j in set(products):
+            for j in order:
+                # min_price = min([n['price'] for n in updated_groups[i] if n['product_id'] == j])
+                min_price = min([n['price'] for n in updated_groups[i] if n['product_id'] == j['product_id'] and n['Объем'] == j['Объем']])
+                temp = [r for r in updated_groups[i] if r['price'] == min_price and r['product_id'] == j['product_id']]
+                if temp:
+                    unique_products.append(temp[0])
             updated_groups[i] = unique_products
         else:
             unique_products.append({'order': updated_groups[i]})
@@ -85,47 +99,124 @@ def take_other_groups(order):
         updated_groups[order[0]['spider']] = {'order': order, 'confirmed': False}
         updated_groups['active'] = order[0]['spider']
     else:
-        updated_groups['mixed'] = {'order': order, 'confirmed': False}
+        updated_groups['active'] = 'Mixed'
+        updated_groups['Mixed'] = {'order': order, 'confirmed': False}
 
     updated_groups['order_num'] = order_num
     for i in updated_groups:
         if type(updated_groups[i]) != dict and i in grouped_shops:
+        # if type(updated_groups[i]) != dict and i in grouped_shops and (len(updated_groups[i]) == len(order)):
             updated_groups[i] = {'order': updated_groups[i], 'confirmed': False}
 
     return updated_groups
 
 
+def get_orders():
+    response = requests.get('https://alko.www-technologies.ru/api/orders')
+    response = json.loads(response.text)
+    return response
+
+
+def prepare_orders(order):
+
+    whiskey = read_file('whiskey.json')
+    champagne = read_file('champagne.json')
+    vodka = read_file('vodka.json')
+    cognac = read_file('cognac.json')
+
+    order_dict = dict()
+    order_dict['order'] = order['items']
+    for item in order_dict['order']:
+        found = [k for k in whiskey if 'id' in k.keys() and k['id'] == item['item_id']]
+        if found:
+            found = found[0]
+            new_item = dict()
+            new_item['price'] = item['price']
+            new_item['name'] = found['name']
+            new_item['image'] = found['image']
+            new_item['section'] = found['section']
+            new_item['link'] = found['link']
+            new_item['product_id'] = found['product_id']
+            new_item['Объем'] = found['features']['Объем']
+            new_item['quantity'] = item['quantity']
+            new_item['spider'] = found['spider']
+
+            order_dict['order'][order_dict['order'].index(item)] = new_item
+
+    order['order_num'] = order.pop('order_id')
+    order['items'] = order_dict['order']
+    order_head_name = list(set([i['spider'] for i in order['items']]))
+    if len(order_head_name) == 1:
+        order[order_head_name[0]] = order.pop('items')
+        order['active'] = order_head_name[0]
+    else:
+        order['Mixed'] = order.pop('items')
+        order['active'] = 'Mixed'
+
+    return order
+
+
+# orders = read_file('orders.json')
+# new_one = {'order_num': '11',
+#           'active': 'Mixed',
+#           'Mixed': [{'image': 'https://amwine.ru/upload/iblock/f0d/f0d4bb11af6ae7c8fe57ca4176a6f00e.png',
+#                     'link': 'https://amwine.ru/catalog/krepkie_napitki/viski/makkalan_dabl_kask_18_let_v_p_u/',
+#                     'name': 'Виски Маккалан дабл каск 18 лет в п/у 0.7 л',
+#                     'price': 11635,
+#                     'product_id': '4185023',
+#                     'quantity': 1,
+#                     'section': 'Виски',
+#                     'spider': 'Amwine',
+#                     'Объем': 0.7},
+#                    {'image': 'https://static.decanter.ru/image/292916-viski-fox-and-dogs-0-5-l-f.jpg',
+#                     'link': 'https://decanter.ru/product/fox-and-dogs-id83074',
+#                     'name': 'Виски Fox and Dogs 0.5 л',
+#                     'price': 504,
+#                     'product_id': '2093337',
+#                     'quantity': 1,
+#                     'section': 'Виски',
+#                     'spider': 'Decanter',
+#                     'Объем': 0.5},
+#                    {'image': 'https://static.decanter.ru/image/300109-viski-fox-and-dogs-0-7-l-f.jpg',
+#                     'link': 'https://decanter.ru/product/fox-and-dogs-id109008',
+#                     'name': 'Виски Fox and Dogs 0.7 л',
+#                     'price': 706,
+#                     'product_id': '2093337',
+#                     'quantity': 1,
+#                     'section': 'Виски',
+#                     'spider': 'Decanter',
+#                     'Объем': 0.7}]}
+
+# orders.append(new_one)
+# write_file('orders.json', orders)
+
 # К такому виду нужно привести заказ для дальнейшей обработки
-order = {"active": "mixed",
-        "order_num": "1872658",
-        "mixed": [{
-            "price": 3634,
-            "name": "Коньяк \"Hennessy\" V.S.O.P., with gift box, 0.7 л",
-            "image": "https://s.wine.style/images_gen/225/2253/0_0_prod_desktop.jpg",
-            "link": "https://winestyle.ru/products/Hennessy-VSOP-Privilege-700.html",
-            "product_id": "2073930",
-            "Объем": 0.7,
-            "quantity": 2,
-            "section": "Коньяк",
-            "spider": "Winestyle"
-          },
-          {
-            "price": 1215,
-            "name": "Водка Русский Стандарт Платинум 1 л",
-            "image": "https://static.decanter.ru/image/305139-vodka-russian-standard-platinum-1-l-f.jpg",
-            "link": "https://decanter.ru/product/russian-standard-platinum-id1493",
-            "product_id": "2080445",
-            "Объем": 1,
-            "quantity": 1,
-            "section": "Водка",
-            "spider": "Decanter"
-          }]
-}
+# order = {"active": "mixed",
+#         "order_num": "1872658",
+#         "mixed": [{
+#             "price": 3634,
+#             "name": "Коньяк \"Hennessy\" V.S.O.P., with gift box, 0.7 л",
+#             "image": "https://s.wine.style/images_gen/225/2253/0_0_prod_desktop.jpg",
+#             "link": "https://winestyle.ru/products/Hennessy-VSOP-Privilege-700.html",
+#             "product_id": "2073930",
+#             "Объем": 0.7,
+#             "quantity": 2,
+#             "section": "Коньяк",
+#             "spider": "Winestyle"
+#           },
+#           {
+#             "price": 1215,
+#             "name": "Водка Русский Стандарт Платинум 1 л",
+#             "image": "https://static.decanter.ru/image/305139-vodka-russian-standard-platinum-1-l-f.jpg",
+#             "link": "https://decanter.ru/product/russian-standard-platinum-id1493",
+#             "product_id": "2080445",
+#             "Объем": 1,
+#             "quantity": 1,
+#             "section": "Водка",
+#             "spider": "Decanter"
+#           }]
+# }
 
-
-# pprint(take_other_groups([order]))
-
-# pprint()
 
 # {'Decanter': {'confirmed': False,
 #               'order': [{'image': 'https://static.decanter.ru/image/300885-kon-iak-hennessy-vsop-0-7-l-f.jpg',
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from pymongo import MongoClient
 from pprint import pprint
 from hashlib import sha1
 from create_groups import read_file
-from create_groups import take_other_groups
+from create_groups import take_other_groups, get_orders, prepare_orders
 import os
 
 
@@ -16,21 +16,52 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '12091988BernardoProvencanoToto'
 
 
+@app.route('/order_for_client/<string:order_num>/<path>', methods=['GET', 'POST'])
+def client_page(order_num, path):
+    order = Orders_DB['orders'].find({'_id': order_num})
+    if order[0][path]['confirmed']:
+        order = order[0][path]['order']
+        sum_ = sum([i['price'] * i['quantity'] for i in order])
+        return render_template('client_page.html', order=order, sum_=sum_)
+    else:
+        return render_template('no_orders.html')
+
+
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
 
     # Orders_DB['orders'].delete_many({})
 
+    # Orders_DB['Виски'].delete_many({})
+    # for i in read_file('whiskey.json'):
+    #     Orders_DB['Виски'].update_one({'_id': i['_id']}, {'$set': i}, upsert=True)
+    #
+    # Orders_DB['Водка'].delete_many({})
+    # for i in read_file('vodka.json'):
+    #     Orders_DB['Водка'].update_one({'_id': i['_id']}, {'$set': i}, upsert=True)
+    #
+    # Orders_DB['Шампанское и игристое вино'].delete_many({})
+    # for i in read_file('champagne.json'):
+    #     Orders_DB['Шампанское и игристое вино'].update_one({'_id': i['_id']}, {'$set': i}, upsert=True)
+    #
+    # Orders_DB['Коньяк'].delete_many({})
+    # for i in read_file('cognac.json'):
+    #     Orders_DB['Коньяк'].update_one({'_id': i['_id']}, {'$set': i}, upsert=True)
+
     if 'change_shop' in request.form:
         Orders_DB['orders'].update_one({'_id': request.form['order']}, {'$set': {'active': request.form['change_shop']}}, upsert=True)
 
-    orders = read_file('orders.json')
+    orders = get_orders()
+    # orders = read_file('orders.json')
+    # orders = []
     for i in orders:
+        i = prepare_orders(i)
+        pprint(i)
         order = take_other_groups([i])
         if not list(Orders_DB['orders'].find({'_id': order['order_num']})):
             Orders_DB['orders'].update_one({'_id': order['order_num']}, {'$set': order}, upsert=True)
 
-    shops = ['Alcomarket', 'Amwine', 'Decanter', 'Lwine', 'Winestreet', 'Winestyle']
+    shops = ['Alcomarket', 'Amwine', 'Decanter', 'Lwine', 'Winestreet', 'Winestyle', 'Mixed']
     orders = list(Orders_DB['orders'].find())
 
     order_list = []
@@ -44,6 +75,7 @@ def main_page():
             if i in shops:
                 if i != list(Orders_DB['orders'].find({'_id': order_num}))[0]['active']:
                     group = list(Orders_DB['orders'].find({'_id': order_num}, {i: 1}))[0]
+                    # pprint(group[i])
                     sums[i] = sum([k['price'] * k['quantity'] for k in group[i]['order']])
 
         Orders_DB['orders'].update_one({'_id': order_num}, {'$set': {'sums': sums}}, upsert=True)
@@ -56,6 +88,7 @@ def main_page():
         new_order['sums'] = sums
         if item[item['active']]['confirmed'] and not [n for n in item[item['active']]['order'] if 'not_in_stock' in n.keys()]:
             new_order['status'] = 'Подтвержден'
+            new_order['client_link'] = 'http://127.0.0.1:5000/order_for_client/' + order_num + '/' + shop
         elif item[item['active']]['confirmed'] and [n for n in item[item['active']]['order'] if 'not_in_stock' in n.keys()]:
             new_order['status'] = 'Нет в наличии'
         elif not item[item['active']]['confirmed']:
@@ -70,16 +103,45 @@ def main_page():
 def orders(order_num, path):
 
     order = list(Orders_DB['orders'].find({'_id': order_num}))
+
     if not order:
         return render_template('no_orders.html')
+    shops = ['Alcomarket', 'Amwine', 'Decanter', 'Lwine', 'Winestreet', 'Winestyle', 'Mixed']
+    order_count = [i for i in order[0].keys() if i in shops]
+    actual_shops = []
+    for i in order_count:
+        for j in order[0][i]['order']:
+            if 'not_in_stock' in j:
+                break
+        else:
+            actual_shops.append(i)
+
     order_num = order[0]['order_num']
     order = order[0][path]
-    # orders = [order]
 
     ip_address = request.remote_addr.replace('.', '')
     # Orders_DB['orders'].delete_many({})
     # Orders_DB['orders'].update_one({'_id': order_num}, {'$set': {'order': order}}, upsert=True)
     # Orders_DB['orders'].update_one({'_id': order_num}, {'$set': {'confirmed': False}}, upsert=True)
+
+    if request.method == 'GET':
+        Orders_DB['replace'].delete_one({'_id': order_num})
+    if 'index' in request.form:
+        # Orders_DB['replace'].delete_many({})
+        replace_list = list(Orders_DB['replace'].find({'_id': order_num}))
+        if not replace_list:
+            Orders_DB['replace'].update_one({'_id': order_num}, {'$set': {'replace_list': [request.form['index']]}}, upsert=True)
+        else:
+            replace_list = replace_list[0]['replace_list']
+            if not (request.form['index'] in replace_list):
+                replace_list.append(request.form['index'])
+            else:
+                replace_list.remove(request.form['index'])
+
+            Orders_DB['replace'].update_one({'_id': order_num}, {'$set': {'replace_list': replace_list}})
+
+        # print('replace_list')
+        # pprint(list(Orders_DB['replace'].find({'_id': order_num})))
 
     if 'comment' in request.form:
         # active_shop = list(Orders_DB['orders'].find({'_id': order_num}))[0]['active']
@@ -102,8 +164,11 @@ def orders(order_num, path):
                 if i['link'] == request.form['not_in_stock']:
                     if not ('not_in_stock' in i.keys()):
                         i['not_in_stock'] = True
+                        Orders_DB[i['section']].update_one({'link': i['link']}, {'$set': {'not_in_stock': True}})
                     else:
                         i.pop('not_in_stock')
+                        Orders_DB[i['section']].update_one({'link': i['link']}, {'$unset': {'not_in_stock': 1}}, upsert=True)
+
             Orders_DB['orders'].delete_one({'_id': order_num})
             Orders_DB['orders'].update_one({'_id': order_num}, {'$set': found[0]}, upsert=True)
 
@@ -114,7 +179,7 @@ def orders(order_num, path):
             i['price'] = float(i['price']) if '.' in i['price'] else int(i['price'])
 
         if orders[0][path]['confirmed']:
-                orders[0][path]['confirmed'] = False
+            orders[0][path]['confirmed'] = False
         else:
             orders[0][path]['confirmed'] = True
 
@@ -123,15 +188,130 @@ def orders(order_num, path):
 
     orders = list(Orders_DB['orders'].find({'_id': order_num}))
     orders = [orders[0][path]['order']]
+
     sum_ = sum([i['price'] * i['quantity'] for i in orders[0] if not ('not_in_stock' in i.keys())])
 
     confirmed = list(Orders_DB['orders'].find({'_id': order_num}))[0]
     confirmed = confirmed[path]['confirmed']
     shop = list(Orders_DB['orders'].find({'_id': order_num}))[0][path]
-
     comments = shop['comments'] if 'comments' in shop.keys() else []
 
-    return render_template('orders.html', groups=orders, order_num=order_num, sum_=sum_, comments=comments[::-1], my_ip=ip_address, confirmed=confirmed, shop=path)
+    not_in_stock = [i for i in orders[0] if 'not_in_stock' in i.keys()]
+    other_drinks = []
+
+    if confirmed and not_in_stock and not actual_shops:
+        # champagne = read_file('champagne.json')
+        # whiskey = read_file('whiskey.json')
+        # vodka = read_file('vodka.json')
+        # cognac = read_file('cognac.json')
+        # drinks = {'Виски': whiskey, 'Водка': vodka, 'Шампанское и игристое вино': champagne, 'Коньяк': cognac}
+
+        for i in not_in_stock:
+            other_drink = {}
+
+            # other_drink[i['name']] = [k for k in drinks[i['section']] if 'product_id' in k.keys() and (k['product_id'] == i['product_id']) and (k['link'] != i['link']) and ('features' in k.keys() and 'Объем' in k['features'].keys() and k['features']['Объем'] == i['Объем'])]
+            other_drink[i['name']] = [k for k in list(Orders_DB[i['section']].find()) if 'product_id' in k.keys() and (k['product_id'] == i['product_id']) and (k['link'] != i['link']) and ('features' in k.keys() and 'Объем' in k['features'].keys() and k['features']['Объем'] == i['Объем']) and not ('not_in_stock' in k.keys())]
+            other_drinks.append(other_drink)
+
+        if 'saving_replace' in request.form:
+
+            replace_list = list(Orders_DB['replace'].find({'_id': order_num}))
+            if replace_list:
+                replace_list = replace_list[0]['replace_list']
+
+            extended_other_drinks = []
+            for i in other_drinks:
+                extended_other_drinks.extend(list(i.values())[0])
+
+            product_id_list = []
+            for i in replace_list:
+                product_id_list.append([k['product_id'] for k in extended_other_drinks if k['link'] == i][0])
+
+            if len(product_id_list) != len(set(product_id_list)):
+                print('Вы можете выбрать один напиток из каждой группы')
+            else:
+                orders_new = list(Orders_DB['orders'].find({'_id': order_num}))[0][path]['order']
+                orders_new_test = list(Orders_DB['orders'].find({'_id': order_num}))
+                not_in_stock_items = [i for i in orders_new if 'not_in_stock' in i.keys()]
+
+                for i in replace_list:
+                    new_product_dict = {}
+                    found = [k for k in extended_other_drinks if k['link'] == i][0]
+                    new_product_dict['image'] = found['image']
+                    new_product_dict['link'] = found['link']
+                    new_product_dict['name'] = found['name']
+                    new_product_dict['section'] = found['section']
+                    new_product_dict['price'] = found['price']
+                    new_product_dict['product_id'] = found['product_id']
+                    new_product_dict['Объем'] = found['features']['Объем']
+                    new_product_dict['quantity'] = [k['quantity'] for k in not_in_stock_items if k['product_id'] == found['product_id']][0]
+                    new_product_dict['price'] = found['price']
+                    new_product_dict['spider'] = found['spider']
+                    orders_new.append(new_product_dict)
+
+                    for k in other_drinks:
+                        if found in list(k.values())[0]:
+                            orders_new.remove([n for n in not_in_stock_items if n['name'] == list(k.keys())[0]][0])
+
+                for i in not_in_stock_items:
+
+                    try:
+                        new_card = {}
+                        # new_card['image'] = request.form[i['name'] + 'image']
+                        new_card['image'] = i['image']
+                        new_card['name'] = request.form[i['name'] + 'name']
+                        new_card['Объем'] = i['Объем']
+                        new_card['price'] = request.form[i['name'] + 'price']
+                        new_card['price'] = float(new_card['price']) if '.' in new_card['price'] else int(new_card['price'])
+                        new_card['spider'] = request.form[i['name'] + 'spider']
+                        new_card['link'] = request.form[i['name'] + 'link']
+                        new_card['quantity'] = i['quantity']
+                        new_card['section'] = i['section']
+                        new_card['product_id'] = i['product_id']
+
+                        orders_new.append(new_card)
+                        orders_new.remove(i)
+                    except:
+                        pass
+
+                if len(set([i['spider'] for i in orders_new])) > 1:
+                    orders_new_test[0].pop(orders_new_test[0]['active'])
+                    orders_new_test[0]['Mixed'] = dict()
+                    orders_new_test[0]['Mixed']['order'] = orders_new
+                    orders_new_test[0]['active'] = 'Mixed'
+                    if not [k for k in orders_new if 'not_in_stock' in k.keys()]:
+                        orders_new_test[0]['Mixed']['confirmed'] = False
+                    else:
+                        orders_new_test[0]['Mixed']['confirmed'] = True
+                    Orders_DB['orders'].delete_one({'_id': order_num})
+                    Orders_DB['orders'].update_one({'_id': order_num}, {'$set': orders_new_test[0]}, upsert=True)
+
+                else:
+                    orders_new_test[0].pop(orders_new_test[0]['active'])
+                    active = list(set([i['spider'] for i in orders_new]))[0]
+                    orders_new_test[0][active] = dict()
+                    orders_new_test[0][active]['order'] = orders_new
+                    orders_new_test[0]['active'] = active
+                    if not [k for k in orders_new if 'not_in_stock' in k.keys()]:
+                        orders_new_test[0][active]['confirmed'] = False
+                    else:
+                        orders_new_test[0][active]['confirmed'] = True
+                    Orders_DB['orders'].delete_one({'_id': order_num})
+                    Orders_DB['orders'].update_one({'_id': order_num}, {'$set': orders_new_test[0]}, upsert=True)
+
+            Orders_DB['replace'].delete_one({'_id': order_num})
+
+            active = list(Orders_DB['orders'].find({'_id': order_num}))[0]['active']
+            orders = list(Orders_DB['orders'].find({'_id': order_num}))
+            orders = [orders[0][active]['order']]
+            sum_ = sum([i['price'] * i['quantity'] for i in orders[0] if not ('not_in_stock' in i.keys())])
+            confirmed = list(Orders_DB['orders'].find({'_id': order_num}))[0]
+            confirmed = confirmed[active]['confirmed']
+            shop = list(Orders_DB['orders'].find({'_id': order_num}))[0][active]
+            comments = shop['comments'] if 'comments' in shop.keys() else []
+            return redirect(f'/order/{order_num}/{active}')
+
+    return render_template('orders.html', groups=orders, order_num=order_num, sum_=sum_, comments=comments[::-1], my_ip=ip_address, confirmed=confirmed, shop=path, other_drinks=other_drinks)
 
 
 if __name__ == '__main__':
